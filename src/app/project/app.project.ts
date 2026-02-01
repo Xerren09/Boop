fda9469cb80ad606776b1cd8f02ce20efcdecfd3
Bars Margetsch <45538919+Xerren09@users.noreply.github.com>
2026-02-01 17:22:44 +0200

    Add app project indexPath property

--- src/app/project/app.project.ts
+++ src/app/project/app.project.ts
@@ -1,10 +1,19 @@
 import { getWorkflowFile, parseWorkflow } from "../workflow.js";
 import { BoopProject, type ProjectConfig } from "./boop.project.js"
 import { createAppRouter } from "../routers/app.router.js";
+import path from "path";
 
 export class AppProject extends BoopProject {
     public override get deployed(): boolean {
         return this._router != undefined;
+    }
+
+    protected _indexPath: string = "";
+    /**
+     * The filesystem path pointing to the project's `index.html` file.
+     */
+    public get indexPath(): string {
+        return this._indexPath;
     }
 
     constructor(config: ProjectConfig){
@@ -13,6 +22,7 @@ export class AppProject extends BoopProject {
 
     protected override async _deploy(): Promise<void> {
         const entryPoint = (await parseWorkflow(await getWorkflowFile(this.binDir))).deploy.entry;
+        this._indexPath = path.join(this.binDir, entryPoint);
         this._router = createAppRouter(this.name, this.binDir, entryPoint);
     }
 
