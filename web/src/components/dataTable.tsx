b40def393ec40b8d88293ac4cbbd1f09b871771c
Bars <45538919+Xerren09@users.noreply.github.com>
2026-05-04 02:35:13 +0300

    Refactor DataTable component

--- web/src/components/dataTable.tsx
+++ web/src/components/dataTable.tsx
@@ -1,58 +1,78 @@
-import { Stack, Text } from "@fluentui/react";
+import { Text } from "@fluentui/react-components";
 import React from "react";
+import Stack from "./stack";
 
-const tdStyle: React.CSSProperties = {
+const tableHeadColStyle: React.CSSProperties = {
     textAlign: "start",
     padding: 0
 };
 
-const tdSeparatorStyle: React.CSSProperties = {
-    ...tdStyle,
+const tableContentColStyle: React.CSSProperties = {
+    textAlign: "start",
+    padding: 0,
+    width: "100%"
+};
+
+const tableSeparatorStyle: React.CSSProperties = {
+    ...tableHeadColStyle,
     paddingLeft: 8,
     paddingRight: 8
 };
 
-export class DataTable extends React.Component<DataTableProps> {
-
-    render(): React.ReactNode {
-        return (
-            <Stack
-                horizontalAlign="start"
+export default function DataTable(props:DataTableProps) {
+    return (
+        <Stack
+            horizontalAlign="start"
+            style={{
+                width: "100%",
+                paddingTop: 12,
+                paddingBottom: 12,
+                borderWidth: 1,
+                borderStyle: "solid",
+                borderColor: "rgb(204, 204, 204)",
+                borderLeft: "none",
+                borderRight: "none"
+            }}
+        >
+            <table
                 style={{
-                    width: "100%",
-                    paddingTop: 12,
-                    paddingBottom: 12,
-                    borderWidth: 1,
-                    borderStyle: "solid",
-                    borderColor: "rgb(204, 204, 204)",
-                    borderLeft: "none",
-                    borderRight: "none"
+                    borderSpacing: `${props.gap ?? 0}px`,
+                    display: "table"
                 }}
             >
-                <table
-                    style={{
-                        borderCollapse: "collapse"
-                    }}
-                >
-                    <tbody>
-                        {
-                            this.props.rows.map((row, index) => (
-                                <tr key={index}>
-                                    <td style={tdStyle}><Text>{ row.label }</Text></td>
-                                    <td style={tdSeparatorStyle}><Text>:</Text></td>
-                                    <td style={tdStyle}> { row.content } </td>
-                                </tr>
-                            ))
-                        }
-                    </tbody>
-                </table>
-            </Stack>
-        )
-    }
+                <tbody>
+                    {
+                        props.rows.map((row, index) => (
+                            <DataTableRow key={`${row.label}-${index}`} label={row.label} separator={ props.separator }>{ row.content }</DataTableRow>
+                        ))
+                    }
+                    {
+                        props.children
+                    }
+                </tbody>
+            </table>
+        </Stack>
+    )
 }
 
-interface DataTableProps {
-    rows: DataTableRow[]
+export function DataTableRow(props: DataTableRowProps) {
+    return (
+        <tr>
+            <td style={tableHeadColStyle}><Text wrap={false}>{ props.label }</Text></td>
+            <td style={tableSeparatorStyle}><Text>{ props.separator ?? ":" }</Text></td>
+            <td style={tableContentColStyle}> { props.children } </td>
+        </tr>
+    )
+}
+interface DataTableProps extends React.PropsWithChildren {
+    rows: DataTableRow[],
+    gap?: number,
+    separator?: string
+}
+
+interface DataTableRowProps extends React.PropsWithChildren {
+    label: string,
+    separator?: string
 }
 
 export interface DataTableRow {
