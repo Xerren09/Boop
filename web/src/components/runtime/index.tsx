428d632a30fdaa471cdf44b760ec81f18957777b
Bars <45538919+Xerren09@users.noreply.github.com>
2026-05-03 17:17:43 +0300

    Replace react-timeago with custom component
    Yeah this was hell but at least its not homophobic

---
+++ web/src/components/runtime/index.tsx
@@ -0,0 +1,90 @@
+import { useEffect, useMemo, useState } from "react";
+import { Text, Tooltip } from "@fluentui/react-components";
+
+function calcInitialValue(props: Props) {
+    const start = typeof (props.start) === "number" ? props.start : props.start.getTime();
+    const end = typeof (props.end) === "number" ? props.end : props.end?.getTime();
+
+    const ret = Math.floor((end == undefined ? (Date.now() - start) : (end - start)) / 1000);
+    return ret;
+}
+
+function getTimeString(counter: number, short?: boolean) {
+    const minute = Math.floor(counter / 60);
+    const hour = Math.floor(counter / 3600);
+    const day = Math.floor(counter / 86400);
+    const year = Math.floor(counter / 31536000);
+
+    if (counter <= 60) 
+        return short ? `${counter}s` : `${counter} seconds`
+    if ((counter > 60) && (counter < 3600)) 
+        return short ? `${String(minute).padStart(2, "0")}:${String(Math.floor(counter % 60)).padStart(2, "0")}` : `${minute} minute${minute > 1 ? "s" : ""} ${Math.floor(counter % 60)} seconds`
+    if ((counter > 3600) && (counter < 86400))
+        return short ? `${String(hour).padStart(2, "0")}:${String(Math.floor((counter % 3600) / 60)).padStart(2, "0")}:${String(Math.floor(counter % 60)).padStart(2, "0")}` : `${hour} hour${hour > 1 ? "s" : ""} ${Math.floor((counter % 3600) / 60)} minutes`
+    if ((counter > 86400) && ((counter < 31536000) || short == true)) 
+        return short ? `${String(day).padStart(2, "0")}:${String(Math.floor((counter % 86400) / 3600)).padStart(2, "0")}:${String(Math.floor(((counter % 86400) % 3600) / 60)).padStart(2, "0")}:${String(Math.floor((((counter % 86400) % 3600) % 60) % 60)).padStart(2, "0")}` : `${day} day${day > 1 ? "s" : ""} ${Math.floor((counter % 86400) / 3600)} hours`
+    if (counter > 31536000) 
+        return `${year} year${year > 1 ? "s" : ""}`
+    return "";
+}
+
+export default function Runtime(props: Props) {
+    const [counter, setCounter] = useState<number>(() => calcInitialValue(props));
+    
+    const timestamp = useMemo(() => {
+        const date = new Date(props.start);
+        return date.toLocaleString();
+    }, [props.start])
+
+    const timestring = useMemo(() => {
+        return getTimeString(counter, props.short);
+    }, [counter, props.short])
+
+    useEffect(() => {
+        if (props.end === undefined) {
+            const interval = setInterval(() => {
+                setCounter((prev) => prev + 1);
+            }, 1000);
+            return () => clearInterval(interval);
+        }
+    }, [props.end])
+
+    useEffect(() => {
+        //eslint-disable-next-line react-hooks/set-state-in-effect
+        setCounter(() => calcInitialValue(props));
+    }, [props.start])
+
+    return (
+        <Tooltip content={ timestamp } relationship="label">
+            <Text style={props.style}>
+                {
+                    timestring
+                }
+                {
+                    (props.end === undefined) && props.since == true ? " ago" : null
+                }
+                {
+                    props.children
+                }
+            </Text>
+        </Tooltip>
+    )
+}
+
+interface Props extends React.PropsWithChildren {
+    /**
+     * The date or timestamp from which the elapsed time should be calculated from.
+     */
+    start: number | Date,
+    /**
+     * The date or timestamp for the end of time frame. If given the component will show the static duration between {@link start} and this time.
+     */
+    end?: number | Date,
+    /**
+     * If set the text will read "<time> ago", instead of just the time. Ignored when {@link end} is set.
+     */
+    since?: boolean,
+    // TODO: short
+    short?: boolean,
+    style?: React.CSSProperties
+}
