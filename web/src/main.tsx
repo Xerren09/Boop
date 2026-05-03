46518f24f655e78563a729448ac7e56257316c28
Bars <45538919+Xerren09@users.noreply.github.com>
2026-05-03 14:29:37 +0300

    Add VITE main.tsx

---
+++ web/src/main.tsx
@@ -0,0 +1,39 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from "react-router";
+import { FluentProvider, webDarkTheme, webLightTheme } from '@fluentui/react-components';
+import './index.css'
+import { FrontPage } from './pages/front/index.tsx';
+import { ProjectPage } from './pages/project/index.tsx';
+import { BoopAPI } from './api/api.ts';
+import Stack from './components/stack/index.tsx';
+
+if (import.meta.env.DEV) {
+    BoopAPI.setOrigin("http://localhost:8004");
+}
+
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <FluentProvider theme={webDarkTheme}>
+            <div
+                style={{
+                    width: "100vw",
+                    height: "100vh"
+                }}
+            >
+                <Stack
+                    horizontalAlign="center"
+                >
+                    <Stack gap={16} style={{ maxWidth: "75vw" }}>
+                        <BrowserRouter>
+                            <Routes>
+                                <Route path="/" Component={FrontPage}/>
+                                <Route path="/:projectId" Component={ProjectPage}/>
+                            </Routes>
+                        </BrowserRouter>
+                    </Stack>
+                </Stack>
+            </div>
+        </FluentProvider>
+    </StrictMode>
+)
