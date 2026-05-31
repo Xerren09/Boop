d7dff3392c1b3e9a0692e0f143f29f608d8fdf6d
Bars <45538919+Xerren09@users.noreply.github.com>
2026-05-31 17:58:32 +0300

    Fix webhook events table formatting

--- web/src/pages/project/eventsTab.tsx
+++ web/src/pages/project/eventsTab.tsx
@@ -1,4 +1,4 @@
-import { Text, Link, Tooltip, Table, TableHeaderCell, TableHeader, TableRow, TableBody, TableCell } from "@fluentui/react-components";
+import { Text, Link, Tooltip, Table, TableHeaderCell, TableHeader, TableRow, TableBody, TableCell, TableCellLayout, useApplyScrollbarWidth } from "@fluentui/react-components";
 import Stack from "../../components/stack";
 import { useContext, useEffect, useMemo, useState } from "react";
 import Runtime from "../../components/runtime";
@@ -13,21 +13,31 @@ const HeaderCells: HeaderCell[] = [
     },
     {
         id: "commit",
-        label: "Commit"
+        label: "Commit",
+        style: {
+            width: 120
+        }
     },
     {
         id: "sender",
-        label: "Sender"
+        label: "Sender",
+        style: {
+            width: 120
+        }
     },
     {
         id: "time",
-        label: "Time"
+        label: "Time",
+        style: {
+            width: 120
+        }
     },
 ];
 
 type HeaderCell = {
     id: string,
-    label: string
+    label: string,
+    style?: React.CSSProperties
 }
 
 export default function ProjectWebhookEventsTab() {
@@ -36,7 +46,7 @@ export default function ProjectWebhookEventsTab() {
 
     const headerCells = useMemo(() => {
         return HeaderCells.map((column) => (
-            <TableHeaderCell key={column.id}>
+            <TableHeaderCell key={column.id} style={column.style}>
                 {column.label}
             </TableHeaderCell>
         ))
@@ -46,24 +56,41 @@ export default function ProjectWebhookEventsTab() {
         return events.reverse().map((event) => (
             <TableRow key={event.time}>
                 <TableCell>
-                    <Stack horizontal verticalAlign="center" horizontalFill gap={8}>
-                        {
-                            event.security.valid == false  && <Tooltip content={<Text>Could not verify the authenticty of this event. This may be due to Boop being started with the <code>DISABLE_WEBHOOK_SECURITY</code> environment variable, or if there was no <code>SECRET</code> set.</Text>} relationship="label"><WarningColor fontSize={20}/></Tooltip>
+                    <TableCellLayout
+                        media={
+                            <span>
+                                {
+                                    event.security.valid == false &&
+                                        <Tooltip
+                                            content={
+                                                <Text>Could not verify the authenticty of this event. This may be due to Boop being started with the <code>DISABLE_WEBHOOK_SECURITY</code> environment variable, or if there was no <code>SECRET</code> set.</Text>
+                                            } relationship="label"
+                                        >
+                                            <WarningColor fontSize={20} />
+                                        </Tooltip>
+                                }
+                            </span>
                         }
-                        <Text>{event.type}</Text>
-                    </Stack>
+                        truncate
+                    >
+                        {event.type}
+                    </TableCellLayout>
                 </TableCell>
                 <TableCell>
-                    {
-                        event.commit.id ? (
-                            <Link href={event.commit.url!} target="_blank">{event.commit.id.substring(0, 7)}@{event.repository.branch}</Link>
-                        )
-                            :
-                        "---"
-                    }
+                    <TableCellLayout truncate>
+                        {
+                            event.commit.id ? (
+                                <Link href={event.commit.url!} target="_blank">{event.commit.id.substring(0, 7)}@{event.repository.branch}</Link>
+                            )
+                                :
+                            "---"
+                        }
+                    </TableCellLayout>
                 </TableCell>
                 <TableCell>
-                    <Link href={event.sender.url} target="_blank">{event.sender.name}</Link>
+                    <TableCellLayout truncate>
+                        <Link href={event.sender.url} target="_blank">{event.sender.name}</Link>
+                    </TableCellLayout>
                 </TableCell>
                 <TableCell>
                     <Runtime since start={event.time}></Runtime>
@@ -82,26 +109,30 @@ export default function ProjectWebhookEventsTab() {
         getEvents();
     }, [project]);
 
+    const scrollWidthAlignRef = useApplyScrollbarWidth();
+
     return(
         <Section
+            title="Received events"
             style={{
                 height: "100%"
             }}
         >
             <Stack horizontalFill verticalFill>
-                <Table noNativeElements >
-                    <TableHeader>
+                <Table noNativeElements>
+                    <TableHeader >
                         <TableRow>
                             {
                                 ...headerCells
                             }
+                            <div role="presentation" ref={scrollWidthAlignRef} />
                         </TableRow>
                     </TableHeader>
                     <TableBody
                         style={{
-                            display: "block",
                             overflowY: "auto",
                             height: 440,
+                            width: "100%"
                         }}
                     >
                         {
