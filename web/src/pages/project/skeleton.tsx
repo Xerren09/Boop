ab774442a31f2ec6f429805568d2c5b8cd15859e
Bars <45538919+Xerren09@users.noreply.github.com>
2026-08-13 21:22:52 +0300

    Update Project page skeleton layout

--- web/src/pages/project/skeleton.tsx
+++ web/src/pages/project/skeleton.tsx
@@ -4,30 +4,30 @@ import Stack from "../../components/stack";
 
 export default function ProjectPageSkeleton() {
     return (
-        <Stack horizontalAlign="start" style={{ width: "75vw" }} horizontalFill>
-            <Skeleton style={{width: "100%"}}>
+        <Skeleton style={{ width: "100%" }}>
+            <Stack gap={24}>
                 <Stack gap={24} horizontalFill>
                     <Section>
-                        <Stack horizontal horizontalAlign="start" verticalAlign="center" gap={12} horizontalFill>
+                        <Stack horizontal horizontalAlign="start" gap={8} horizontalFill>
                             <SkeletonItem size={28} shape="circle"/>
                             <SkeletonItem size={28} shape="circle"/>
                             <SkeletonItem size={32} style={{width: "250px"}}/>
                         </Stack>
-                    
+                        
                         <Stack horizontal horizontalFill style={{ marginTop: 36 }}>
                             <Stack horizontal horizontalFill horizontalAlign="space-between" verticalAlign="start" gap={12} >
                                 <Stack horizontal gap={12}>
-                                    <SkeletonItem size={32} style={{width: "90px"}}/>
-                                    <SkeletonItem size={32} style={{width: "90px"}}/>
-                                    <SkeletonItem size={32} style={{width: "90px"}}/>
+                                    <SkeletonItem size={32} style={{width: "10%", minWidth: "46px"}}/>
+                                    <SkeletonItem size={32} style={{width: "10%", minWidth: "46px"}}/>
+                                    <SkeletonItem size={32} style={{width: "10%", minWidth: "46px"}}/>
                                 </Stack>
                                 <Stack horizontal>
-                                    <SkeletonItem size={32} style={{width: "90px"}}/>
+                                    <SkeletonItem size={32} style={{width: "10%", minWidth: "46px"}}/>
                                 </Stack>
                             </Stack>
                         </Stack>
                         <Divider style={{marginTop: 16, marginBottom: 12}}/>
-                        <Stack gap={12} horizontalFill>
+                        <Stack gap={6} horizontalFill>
                             <SkeletonItem size={16} />
                             <SkeletonItem size={16} />
                             <SkeletonItem size={16} />
@@ -37,17 +37,20 @@ export default function ProjectPageSkeleton() {
                         <Divider style={{marginTop: 16, marginBottom: 12}}/>
                     </Section>
                 </Stack>
+                <Stack gap={12} horizontalFill horizontalAlign="start" horizontal>
+                    <SkeletonItem size={24} style={{ width: "70px" }} />
+                    <SkeletonItem size={24} style={{ width: "70px" }} />
+                    <SkeletonItem size={24} style={{ width: "70px" }} />
+                    <SkeletonItem size={24} style={{ width: "70px" }} />
+                </Stack>
                 <Section>
-                    <Stack gap={12} style={{marginTop: 12, marginBottom: 24}} horizontalFill horizontalAlign="start" horizontal>
-                        <SkeletonItem size={24} style={{width: "90px"}}/>
-                        <SkeletonItem size={24} style={{width: "90px"}}/>
-                        <SkeletonItem size={24} style={{ width: "90px" }} />
-                        <SkeletonItem size={24} style={{width: "90px"}}/>
+                    <Stack horizontal gap={8} horizontalFill>
+                        <SkeletonItem size={28} shape="circle"/>
+                        <SkeletonItem size={32} style={{width: "250px"}}/>
                     </Stack>
-                    <SkeletonItem size={32} style={{ width: "250px" }} />
-                    <SkeletonItem size={128} style={{ marginTop: 12 }} />
+                    <SkeletonItem size={56} style={{ marginTop: 12 }} />
                 </Section>
-            </Skeleton>
-        </Stack>
+            </Stack>
+        </Skeleton>
     )
 }
